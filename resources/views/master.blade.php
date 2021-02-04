14fc38ab06deb51886eb5b02328d744cca989181
Jwan <jwanaalfatla1999@gmail.com>
2021-02-04 23:35:10 +0300

    First commit

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -6,6 +6,8 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/app.css">
+    <meta name="yandex-verification" content="6e72ede6ef2d9146" />
+
     <link rel="icon" href="robotnarecolor.png">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Tajawal:wght@300;400;500;700&display=swap" rel="stylesheet">
